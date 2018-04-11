bae1739949c8fcde5aab605ce1ac5adf9e2e8521
RichichiDomotics <larryconther@gmail.com>
2018-04-11 16:18:48 -0500

    JPA Integration

--- src/main/java/com/websystique/springmvc/controller/AppController.java
+++ src/main/java/com/websystique/springmvc/controller/AppController.java
@@ -1,6 +1,9 @@
 package com.websystique.springmvc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class AppController {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(AppController.class);
 
 	@RequestMapping(value = { "/"}, method = RequestMethod.GET)
 	public String homePage(ModelMap model) {
@@ -24,5 +30,11 @@ public class AppController {
 	@RequestMapping(value = { "/contactus"}, method = RequestMethod.GET)
 	public String contactUsPage(ModelMap model) {
 		return "contactus";
+	}
+
+	@RequestMapping(value = "/login",method = RequestMethod.GET)
+	public String Login(Model model){
+		logger.info("Vista Login");
+		return "login";
 	}
 }
